aac9e18697ca497987a762f53e0b4774290cfe7e
Marco Ambuludi <41352956+marco1730417@users.noreply.github.com>
2024-01-03 20:30:45 -0500

    CONTROL DE MARACACIONES

--- src/views/marcaciones/Marcaciones.vue
+++ src/views/marcaciones/Marcaciones.vue
@@ -6,49 +6,49 @@ import { useUserStore } from '@/store/user.js';
 
 const auth = useUserStore();
 
-const { datosMarcaciones,guardarRegistroEntrada,guardarRegistroSalida, obtenerRegistrosMarcaciones } = useRestApi() //Instancia composable Rest
+const { datosMarcaciones, guardarRegistroEntrada, guardarRegistroSalida, obtenerRegistrosMarcaciones } = useRestApi() //Instancia composable Rest
 const url = ref('marcaciones');
 
 
 const modalRegistro = ref(false);
 const modalBorrarRegistro = ref(false);
 const marcacion = ref({});
-const fecha_inicio = ref ('');
+const fecha_inicio = ref('');
 const fecha_fin = ref('');
 const dt = ref(null);
 const filters = ref({});
 const submitted = ref(false);
-const usuario= ref(auth.user.id);
+const usuario = ref(auth.user.id);
 
-const dataenviar=ref({usuario_id:usuario.value})
+const dataenviar = ref({ usuario_id: usuario.value })
 
-marcacion.value.usuario_id=usuario.value;
+marcacion.value.usuario_id = usuario.value;
 
 onBeforeMount(() => {
     initFilters();
 });
 onMounted(() => {
 
-    
-    obtenerRegistrosMarcaciones(url.value,dataenviar.value);  
+
+    obtenerRegistrosMarcaciones(url.value, dataenviar.value);
 
 });
 
 
 const guardarRegistrodeEntrada = () => {
-    
-            //Creacion
-            guardarRegistroEntrada(url.value,marcacion.value);
-            obtenerRegistrosMarcaciones(url.value,dataenviar.value);  
- 
-    };
+
+    //Creacion
+    guardarRegistroEntrada(url.value, marcacion.value);
+    obtenerRegistrosMarcaciones(url.value, dataenviar.value);
+
+};
 
 const guardarRegistrodeSalida = () => {
-  
-            //Creacion
-            guardarRegistroSalida(url.value,marcacion.value);
-       
-            obtenerRegistrosMarcaciones(url.value,dataenviar.value);  
+
+    //Creacion
+    guardarRegistroSalida(url.value, marcacion.value);
+
+    obtenerRegistrosMarcaciones(url.value, dataenviar.value);
 
 };
 
@@ -66,36 +66,29 @@ const initFilters = () => {
         <div class="col-12 lg:col-6">
             <div class="card p-fluid">
                 <h5>INGRESO</h5>
-       
+
                 <Button label="Timbrar" icon="pi pi-sign-in" style="width: auto" @click="guardarRegistrodeEntrada" />
             </div>
-  
+
         </div>
         <div class="col-12 lg:col-6">
             <div class="card p-fluid">
                 <h5>SALIDA</h5>
-                <Button label="Timbrar" icon="pi pi-sign-out" class="p-button-danger" style="width: auto" @click="guardarRegistrodeSalida" />
-             
+                <Button label="Timbrar" icon="pi pi-sign-out" class="p-button-danger" style="width: auto"
+                    @click="guardarRegistrodeSalida" />
+
             </div>
 
         </div>
 
         <div class="col-12 lg:col-12">
             <div class="card">
-         
-                <DataTable
-                    ref="dt"
-                    :value="datosMarcaciones"
-                    dataKey="id"
-                    :paginator="true"
-                    :rows="10"
-                    :filters="filters"
-                 
+
+                <DataTable ref="dt" :value="datosMarcaciones" dataKey="id" :paginator="true" :rows="10" :filters="filters"
                     paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
                     :rowsPerPageOptions="[5, 10, 25]"
                     currentPageReportTemplate="Mostrando {first} hasta {last} de {totalRecords} registros"
-                    responsiveLayout="scroll"
-                >
+                    responsiveLayout="scroll">
                     <template #header>
                         <div class="flex flex-column md:flex-row md:justify-content-between md:align-items-center">
                             <h5 class="m-0">Historial de Marcaciones</h5>
@@ -106,32 +99,35 @@ const initFilters = () => {
                         </div>
                     </template>
 
-                <!--     <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
-                 -->    <Column field="nombre" header="FECHA" :sortable="true" headerStyle="width:25%; min-width:10rem;">
+                    <!--     <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
+                 -->
+                    <Column field="nombre" header="FECHA" :sortable="true" headerStyle="width:25%; min-width:10rem;">
                         <template #body="slotProps">
                             <span class="p-column-title">FECHA</span>
 
-                            {{ slotProps.data.fecha_hora_entrada }} 
-                      
+                            {{ slotProps.data.fecha }}
+
                         </template>
                     </Column>
-                    <Column field="identificacion" header="ENTRADA" :sortable="true" headerStyle="width:14%; min-width:10rem;">
+                    <Column field="identificacion" header="ENTRADA" :sortable="true"
+                        headerStyle="width:14%; min-width:10rem;">
                         <template #body="slotProps">
                             <span class="p-column-title">ENTRADA</span>
-                            {{ slotProps.data.fecha_hora_entrada }}
+                            {{ slotProps.data.hora_entrada }}
                         </template>
                     </Column>
-                    <Column field="identificacion" header="SALIDA" :sortable="true" headerStyle="width:14%; min-width:10rem;">
+                    <Column field="identificacion" header="SALIDA" :sortable="true"
+                        headerStyle="width:14%; min-width:10rem;">
                         <template #body="slotProps">
                             <span class="p-column-title">SALIDA</span>
-                            {{ slotProps.data.fecha_hora_salida }}
+                            {{ slotProps.data.hora_salida }}
                         </template>
                     </Column>
-            
+
                 </DataTable>
             </div>
         </div>
-     
+
     </div>
 </template>
 
