f735abc2d89c911820e0a562692e22d59ec191f4
Marco Ambuludi <41352956+marco1730417@users.noreply.github.com>
2024-05-24 19:28:18 -0500

    HORAS EXTRAS / MOSTRAR SUMATORIA

--- src/views/reportes/HorasExtras.vue
+++ src/views/reportes/HorasExtras.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { FilterMatchMode } from 'primevue/api';
-import { ref, onMounted, onBeforeMount } from 'vue';
+import { ref, onMounted, onBeforeMount, computed } from 'vue';
 import { useRestApi } from '@/composables/crud';
 
-const { cargarAgendamiento, datosAgendamiento, calcularMarcaciones, datosReporteMarcaciones, obtenerRegistros,datos } = useRestApi() //Instancia composable Rest
+const { cargarAgendamiento, datosAgendamiento, calcularMarcaciones, datosReporteMarcaciones, obtenerRegistros, datos } = useRestApi() //Instancia composable Rest
 
 const url = ref('reportes');
 const urlusuarios = ref('usuarios');
@@ -39,7 +39,7 @@ const cliente = ref({});
 const dt = ref(null);
 const filters = ref({});
 
-const datosenviar = ref({ mes: selectedMes, anio: selectedAnio , usuario_id:selectedUsuario })
+const datosenviar = ref({ mes: selectedMes, anio: selectedAnio, usuario_id: selectedUsuario })
 
 const submitted = ref(false);
 
@@ -47,9 +47,21 @@ const submitted = ref(false);
 onBeforeMount(() => {
     initFilters();
 });
+
+
+
+// Propiedad computada para sumar todos los valores de tiempo_excedente
+const sumaValorTotal = computed(() => {
+    return datosReporteMarcaciones.value.reduce((total, item) => {
+        console.log(item.valor_pagar)
+        if(item.valor_pagar>0)   total = total+ item.valor_pagar;
+        return total
+    }, 0);
+});
+
 onMounted(() => {
 
-    obtenerRegistros(urlusuarios.value);   
+    obtenerRegistros(urlusuarios.value);
 });
 
 const generarReporte = () => {
@@ -82,53 +94,62 @@ const initFilters = () => {
         <div class="col-12">
             <div class="card">
                 <Toast />
-                
-            <div class="grid p-fluid">
-        <div class="col">
-            <div class="card">
-                <h5>Generación de reporte Horas Extras</h5>
-                <div class="p-fluid grid">
-                    <div class="col-9 md:col-3">
-                        <div class="field">
-                            <label for="inputtext">Elija el Año</label>
-                            <Dropdown v-model="selectedAnio" :options="anios" optionLabel="name"
-                                    placeholder="Eliga un año" class="w-full md:w-14rem" />
-                        </div>
-                   
-                    </div>
 
-                    <div class="col-9 md:col-3">
-                        <div class="field">
-                            <label for="inputmask">Elija el Mes</label>
-                            <Dropdown v-model="selectedMes" :options="mes" optionLabel="name" placeholder="Eliga un mes"
-                                    class="w-full md:w-14rem" />
-                            
-                        </div>
+                <div class="grid p-fluid">
+                    <div class="col">
+                        <div class="card">
+                            <h5>Generación de reporte Horas Extras</h5>
                 
-                    </div>
-                    <div class="col-9 md:col-3">
-                        <div class="field">
-                            <label for="inputmask">Elija el Usuario</label>
-                            <Dropdown v-model="selectedUsuario" :options="datos" optionLabel="name" placeholder="Eliga un usuario"
+                            <div class="p-fluid grid">
+                                <div class="col-9 md:col-3">
+                                    <div class="field">
+                                        <label for="inputtext">Elija el Año</label>
+                                        <Dropdown v-model="selectedAnio" :options="anios" optionLabel="name"
+                                            placeholder="Eliga un año" class="w-full md:w-14rem" />
+                                    </div>
 
-class="w-full md:w-14rem" />      
-                        </div>
-                
-                    </div>
-                    <div class="col-9 md:col-3">
-                        <div class="field">
-                            <label for="inputmask">Acciones</label>
-                            <Button label="Consultar" icon="pi pi-calculator" class="p-button-success" @click="generarReporte" />
+                                </div>
+
+                                <div class="col-9 md:col-3">
+                                    <div class="field">
+                                        <label for="inputmask">Elija el Mes</label>
+                                        <Dropdown v-model="selectedMes" :options="mes" optionLabel="name"
+                                            placeholder="Eliga un mes" class="w-full md:w-14rem" />
+
+                                    </div>
+
+                                </div>
+                                <div class="col-9 md:col-3">
+                                    <div class="field">
+                                        <label for="inputmask">Elija el Usuario</label>
+                                        <Dropdown v-model="selectedUsuario" :options="datos" optionLabel="name"
+                                            placeholder="Eliga un usuario" class="w-full md:w-14rem" />
+                                    </div>
+
+                                </div>
+                                <div class="col-9 md:col-3">
+                                    <div class="field">
+                                        <label for="inputmask">Acciones</label>
+                                        <Button label="Consultar" icon="pi pi-calculator" class="p-button-success"
+                                            @click="generarReporte" /> <br><br>
+                                            <div>
+                                                <Button  :label="sumaValorTotal" icon="pi pi-dollar" class="p-button-warning" badgeClass="p-badge-danger"></Button>
               
+                                            </div>
+                                            
+
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
-                
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-          
-                <DataTable ref="dt" :value="datosReporteMarcaciones" dataKey="id" :paginator="true" :rows="10" :filters="filters"
+                
+                
+
+                <DataTable ref="dt" :value="datosReporteMarcaciones" dataKey="id" :paginator="true" :rows="10"
+                    :filters="filters"
                     paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
                     :rowsPerPageOptions="[5, 10, 25]"
                     currentPageReportTemplate="Mostrando {first} hasta {last} de {totalRecords} registros"
@@ -175,27 +196,30 @@ class="w-full md:w-14rem" />
                             {{ slotProps.data.tipo_dia }}
                         </template>
                     </Column>
-                    <Column field="direccion" header="HORAS EXTRAS" :sortable="true" headerStyle="width:15%; min-width:10rem;">
+                    <Column field="direccion" header="HORAS EXTRAS" :sortable="true"
+                        headerStyle="width:15%; min-width:10rem;">
                         <template #body="slotProps">
                             <span class="p-column-title">HE</span>
-                    
-                            <Badge v-if="slotProps.data.valor_pagar < 0" severity="info"> {{ slotProps.data.tiempo_excedente }}</Badge>
-                           
-                            <span v-if="slotProps.data.valor_pagar >= 0" > {{ slotProps.data.tiempo_excedente }}</span>
-                           
+
+                            <Badge v-if="slotProps.data.valor_pagar < 0" severity="info"> {{
+                                slotProps.data.tiempo_excedente }}</Badge>
+
+                            <span v-if="slotProps.data.valor_pagar >= 0"> {{ slotProps.data.tiempo_excedente }}</span>
+
                         </template>
                     </Column>
                     <Column field="direccion" header="PAGO" :sortable="true" headerStyle="width:15%; min-width:10rem;">
                         <template #body="slotProps">
                             <span class="p-column-title">PAGO</span>
-                            <span v-if="slotProps.data.valor_pagar >= 0"> $ {{slotProps.data.valor_pagar.toFixed(2)}}</span>
-                            <span title="No aplica" v-if="slotProps.data.valor_pagar < 0"> N/A</span>
+                            <span v-if="slotProps.data.valor_pagar >= 0"> $
+                                {{ slotProps.data.valor_pagar }}</span>
+                            <span title="No aplica" v-if="slotProps.data.valor_pagar < 0 || slotProps.data.valor_pagar == null"> N/A</span>
                         </template>
-                    </Column> 
+                    </Column>
 
 
 
-               <!--      <Column headerStyle="min-width:10rem;" header="ACCIONES ">
+                    <!--      <Column headerStyle="min-width:10rem;" header="ACCIONES ">
                         <template #body="slotProps">
                             <Button icon="pi pi-sync" class="p-button-rounded p-button-success mr-2"
                                 @click="cambiarEstado(slotProps.data.id)" />
